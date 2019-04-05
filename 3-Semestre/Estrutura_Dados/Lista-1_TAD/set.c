196d7fe334f27dc88dfbfc9a0d12011fc817c6ef
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-04-05 19:19:56 -0300

    Add files via upload

--- 3-Semestre/Estrutura_Dados/Lista-1_TAD/set.c
+++ 3-Semestre/Estrutura_Dados/Lista-1_TAD/set.c
@@ -1,19 +1,20 @@
+/**
+ *
+ * Trabalho 1 - Estruturas de Dados I
+ *
+ * Sobre: Conjunto set
+ *
+ * Integrantes (Ciência da Computação):
+ *
+ * Daniel Bou de Souza
+ * Maurício da Silva Freire
+ * Rodrigo Suarez Moreira
+ *
+ * Data: 01/04/2019
+*/
+
 #include "set.h"
 #include <stdio.h>
-
-/*
-* Trabalho 1 - Estruturas de Dados I
-*
-* Sobre: Conjunto set
-*
-* Integrantes (Ciência da Conputação):
-*
-* Rodrigo Suarez Moreira
-* Daniel Bou de Souza
-* Maurício da Silva Freire
-*
-* Data: 01/04/2019
-*/
 
 void set_init(set * s)
 {
@@ -69,14 +70,14 @@ bool set_remove(set * s, set_info x)
 
 bool set_union(set s1, set s2, set * s3)
 {
-    int k=0,i,j;
+    int k = 0, i, j;
 
-    for (i=0; i < s1.n; i++) {
+    for (i = 0; i < s1.n; i++) {
         if (set_member(s2, s1.itens[i])) {
-            k+=1;
+            k += 1;
         }
     }
-    if ((s1.n + s2.n)-k <= SET_MAX) {
+    if (s1.n + s2.n-k <= SET_MAX) {
        set_init(s3);
        for (i = 0; i < s1.n; i++) {
             set_insert(&*s3, s1.itens[i]);
@@ -91,56 +92,46 @@ bool set_union(set s1, set s2, set * s3)
     return false;
 }
 
-void set_intersection(set s1, set s2, set * s3) {
-
+void set_intersection(set s1, set s2, set * s3)
+{
     int i;
     set_init(&*s3);
-    for (i=0; i < s1.n; i++) {
-        if (set_member(s2,s1.itens[i])) {
+    for (i = 0; i < s1.n; i++) {
+        if (set_member(s2, s1.itens[i])) {
             set_insert(&*s3, s1.itens[i]);
         }
     }
 
 }
 
-bool set_difference(set s1, set s2, set * s3) {
-
-    int k=0,i;
+bool set_difference(set s1, set s2, set * s3)
+{
+    int k = 0,i;
     set_init(&*s3);
-    for (i=0; i < s1.n; i++) {
-        if (!set_member(s2,s1.itens[i])) {
-            k+=1;
+    for (i = 0; i < s1.n; i++) {
+        if (!set_member(s2, s1.itens[i])) {
+            k += 1;
         }
     }
-    for (i=0; i < s2.n; i++) {
-        if (!set_member(s1,s2.itens[i])) {
-            k+=1;
-        }
-    }
+
     if (k <= SET_MAX) {
-        for (i=0; i < s1.n; i++) {
-            if (!set_member(s2,s1.itens[i])) {
+        for (i = 0; i < s1.n; i++) {
+            if (!set_member(s2, s1.itens[i])) {
                 set_insert(&*s3, s1.itens[i]);
-            }
-        }
-        for (i=0; i < s2.n; i++) {
-            if (!set_member(s1,s2.itens[i])) {
-                set_insert(&*s3, s2.itens[i]);
             }
         }
         return true;
     } else {
         return false;
     }
-
 }
 
-void set_print(set s1) {
-
+void set_print(set s1)
+{
     int i;
     printf("[ ");
-    for (i=0; i < s1.n; i++) {
-        printf("%d,", s1.itens[i]);
+    for (i = 0; i < s1.n; i++) {
+        printf("%d, ", s1.itens[i]);
     }
-    printf("\b ]\n");
+    printf("\b\b ]\n");
 }
