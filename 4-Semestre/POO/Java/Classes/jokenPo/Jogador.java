29288f438bd410577c81e51a3fe27ea15bbdbbec
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-10-03 20:55:58 -0300

    Add files via upload

---
+++ 4-Semestre/POO/Java/Classes/jokenPo/Jogador.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jokenPo;
+
+/**
+ *
+ * @author Maurício
+*/
+public class Jogador implements Comparable<Jogador> {
+    private String nome;
+    private int mão;
+    
+    public void setNome(String nm) { this.nome = nm; }
+    public void setMao(int mao) { this.mão = mao; }
+    public String getNome(String nm) { return this.nome; }
+    public int getMao(int nm) { return this.mão; }
+    
+    @Override
+    public int compareTo(Jogador jd) {
+        // 0 - Pedra              
+        // 1 - Papel
+        // 2 - Tesoura
+        switch(this.mão) {
+            case 0:
+                if (jd.mão == 1) return this.mão - jd.mão;
+                else return -(this.mão - jd.mão);
+            case 1:
+                if (jd.mão == 2) return this.mão - jd.mão;
+                else return -(this.mão - jd.mão);
+            case 2:
+                if (jd.mão == 0) return this.mão - jd.mão;
+                else return -(this.mão - jd.mão);
+            default:
+                System.out.println("Jagada Inválida!");
+        }
+        return 8;
+    }
+}
