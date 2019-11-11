474bf7b5b9fa5258d7c618ed93c0cc9beabd357c
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-11-11 19:57:31 -0300

    Add files via upload

--- Prob.c
+++ Prob.c
@@ -6,21 +6,30 @@
 #include <math.h>
 
 double func(double, double, double);
+double tb(double);
 
 int main()
 {
     int n;
-    double x, y, z;
+    double x, y, z, r;
     printf("Digite três números: ");
     scanf("%lf %lf %lf", &x, &y, &z);
 
-    x = func(x, y, z);
-    printf("%lf\n", x);
+    r = func(x, y, z);
+    printf("Na tabela: %lf\n", r);
+    r = tb(x);
+    printf("\nTestando: %lf\n", r);
 }
 
 double func(double x, double y, double z) 
 {
     z = (x - y) / z;
     return z;
+}
+
+double tb(double x)
+{
+    double constant = 1 / sqrt(2 * 3.14);
+    return (constant * 2.718281 * ((-x * -x) / 2)*(-x * -x) / 2);
 }
 
